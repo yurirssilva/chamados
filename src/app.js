34acdb9b897c1d6108513965226688cd799310fa
mastercoks <matheusbdo1@gmail.com>
2019-10-19 09:36:52 -0300

    Adicionando banco remoto

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv/config');
 // const bodyPaser = require('body-paser');
+
+const urlMongo = process.env.DB === 'localhost' ? 
+  `mongodb://localhost:27017/chamados`: 
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds153841.mlab.com:53841/chamados`;
 
 const routes = require('./routes');
 
 const app = express();
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost:27017/chamados', { 
+mongoose.connect(urlMongo, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true 
 });
